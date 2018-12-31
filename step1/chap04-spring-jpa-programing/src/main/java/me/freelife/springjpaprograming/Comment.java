047f7616725c8845ea07539b02423e09c2b2eb7f
freelife <freejava1191@gmail.com>
2018-12-31 23:40:36 +0900

    chap05 Entity Status And Cascade

---
+++ step1/chap04-spring-jpa-programing/src/main/java/me/freelife/springjpaprograming/Comment.java
@@ -0,0 +1,41 @@
+package me.freelife.springjpaprograming;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Comment {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String comment;
+
+    @ManyToOne
+    private Post post;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+}
--- step1/chap04-spring-jpa-programing/src/main/java/me/freelife/springjpaprograming/JpaRunner.java
+++ step1/chap04-spring-jpa-programing/src/main/java/me/freelife/springjpaprograming/JpaRunner.java
@@ -1,5 +1,6 @@
 package me.freelife.springjpaprograming;
 
+import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -17,22 +18,49 @@ public class JpaRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        // Entity 상태 테스트
+        /*
         Account account = new Account();
         account.setUsername("freelife");
         account.setPassword("hibernate");
 
         Study study = new Study();
         study.setName("Spring Data JPA");
-//        study.setOwner(account);
-
-//        account.getStudies().add(study);
-//        study.setOwner(account);
 
         account.addStudy(study);
-        entityManager.persist(account);
-        entityManager.persist(study);
 
-//        Session session = entityManager.unwrap(Session.class);
-//        session.save(account);
+        Session session = entityManager.unwrap(Session.class);
+        session.save(account);
+        session.save(study);
+
+        Account freelife = session.load(Account.class, account.getId());
+        freelife.setUsername("ironman");
+        freelife.setUsername("superman");
+        freelife.setUsername("freelife");
+        System.out.println("=====================");
+        System.out.println(freelife.getUsername());
+        */
+
+        // Post(Parent) Comment(Child) Cascade 테스트 예제
+        /*
+        Post post = new Post();
+        post.setTitle("Spring Data JPA 언제 보나...");
+
+        Comment comment = new Comment();
+        comment.setComment("빨리 보고 싶엉.");
+        post.addComment(comment);
+
+        Comment comment1 = new Comment();
+        comment1.setComment("곧 보여드릴께요.");
+        post.addComment(comment1);
+
+        Session session = entityManager.unwrap(Session.class);
+        session.save(post);
+        */
+
+        // REMOVE 테스트
+        Session session = entityManager.unwrap(Session.class);
+        Post post = session.get(Post.class, 1l);
+        session.delete(post);
     }
 }
