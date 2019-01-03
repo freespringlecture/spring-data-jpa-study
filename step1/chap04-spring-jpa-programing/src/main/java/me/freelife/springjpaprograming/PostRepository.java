e5d30c4d749d662b7b7b0dd5a4f09c05693a4503
freelife <freejava1191@gmail.com>
2019-01-03 10:05:12 +0900

    chap08-1 Repository

--- step1/chap04-spring-jpa-programing/src/main/java/me/freelife/springjpaprograming/PostRepository.java
+++ step1/chap04-spring-jpa-programing/src/main/java/me/freelife/springjpaprograming/PostRepository.java
@@ -1,8 +1,12 @@
 package me.freelife.springjpaprograming;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Page<Post> findByTitleContains(String title, Pageable pageable);
+
+    long countByTitleContains(String title);
 }
