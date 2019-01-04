e21abd46743b7f2160a3c89d1c897503cf4c3398
freelife <freejava1191@gmail.com>
2019-01-04 23:26:11 +0900

    chap08-11-3 Pageable and Sort Parameter

--- step1/chap04-spring-jpa-programing/src/main/java/me/freelife/springjpaprograming/post/PostController.java
+++ step1/chap04-spring-jpa-programing/src/main/java/me/freelife/springjpaprograming/post/PostController.java
@@ -1,6 +1,8 @@
 package me.freelife.springjpaprograming.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,17 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     @Autowired
-    private PostRepository postRepository;
-
-    //생성자가 하나만 있는 경우 자동으로 빈으로 주입을 해줌
-    /*
-    public PostController(PostRepository postRepository) {
-        this.postRepository = postRepository;
-    }
-    */
+    private PostRepository posts;
 
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable("id") Post post) {
         return post.getTitle();
+    }
+
+    @GetMapping("/posts")
+    public Page<Post> getPosts(Pageable pageable) {
+        return posts.findAll(pageable);
     }
 }
--- step1/chap04-spring-jpa-programing/src/test/java/me/freelife/springjpaprograming/post/PostControllerTest.java
+++ step1/chap04-spring-jpa-programing/src/test/java/me/freelife/springjpaprograming/post/PostControllerTest.java
@@ -8,10 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -35,6 +35,24 @@ public class PostControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("jpa"));
+    }
+
+    @Test
+    public void getPosts() throws Exception {
+
+        Post post = new Post();
+        post.setTitle("jpa");
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts")
+                    .param("page", "0")
+                    .param("size", "10")
+                    .param("sort", "created,desc")
+                    .param("sort", "title"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].title", is("jpa")));
+
     }
 
 }
