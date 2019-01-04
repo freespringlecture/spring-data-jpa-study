9ed48c64d166d4dbfc8cf47406bdb9da3eb9e505
freelife <freejava1191@gmail.com>
2019-01-04 19:52:50 +0900

    chap08-9 QueryDSL Customizing

--- step1/chap04-spring-jpa-programing/src/main/java/me/freelife/springjpaprograming/post/PostRepository.java
+++ step1/chap04-spring-jpa-programing/src/main/java/me/freelife/springjpaprograming/post/PostRepository.java
@@ -1,6 +1,7 @@
 package me.freelife.springjpaprograming.post;
 
 import me.freelife.springjpaprograming.MyRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface PostRepository extends MyRepository<Post, Long> {
+public interface PostRepository extends MyRepository<Post, Long>, QuerydslPredicateExecutor<Post> {
 }
---
+++ step1/chap04-spring-jpa-programing/src/test/java/me/freelife/springjpaprograming/post/PostRepositoryTest.java
@@ -0,0 +1,33 @@
+package me.freelife.springjpaprograming.post;
+
+import com.querydsl.core.types.Predicate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@Import(PostRepositoryTestConfig.class)
+public class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    public void crud() {
+        Post post = new Post();
+        post.setTitle("hibernate");
+        postRepository.save(post.publish());
+
+        Predicate predicate = QPost.post.title.containsIgnoreCase("hi");
+        Optional<Post> one = postRepository.findOne(predicate);
+        assertThat(one).isNotEmpty();
+    }
+}
